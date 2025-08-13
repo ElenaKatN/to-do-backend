af2c5e0cce96acdb3a28027298bd39a8befd5758
ElenaKatN <elenan.graphicdesigner@gmail.com>
2025-08-13 17:56:10 -0600

    + adding elements to list actions

--- server.js
+++ server.js
@@ -30,6 +30,22 @@ app.post("/api/todos", (req, res) => {
 });
 
 // Serve frontend in production
+app.put("/api/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const todo = todos.find((t) => t.id == id);
+  if (!todo) return res.status(404).json({ message: "Todo not found" });
 
+  todo.completed = req.body.completed;
+  res.json(todo);
+});
+
+app.delete("/api/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const index = todos.findIndex((t) => t.id == id);
+  if (index === -1) return res.status(404).json({ message: "Todo not found" });
+
+  todos.splice(index, 1);
+  res.status(204).send(); // 204 No Content
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
